f8cf65a03a6e9e38e066186398420adc86d16e0b
tomekjaskowiec <149264856+tomekjaskowiec@users.noreply.github.com>
2025-10-27 15:49:06 +0100

    commit

--- cw3.sql
+++ cw3.sql
@@ -5,13 +5,34 @@ create extension if not exists postgis;
 --1
 
 
-
 select * from t2019_kar_buildings
 except
 select * from t2018_kar_buildings;
 
 
 --2
+
+select st_srid(t2019_kar_buildings.geometry) from t2019_kar_buildings;
+update t2019_kar_buildings
+set geometry = ST_SetSRID(geometry, 3068)
+where st_srid(t2019_kar_buildings.geometry) != 3068;
+
+update t2019_kar_buildings
+set geometry = ST_SetSRID(geometry, 0)
+where st_srid(t2019_kar_buildings.geometry) != 0;
+
+update t2018_kar_buildings
+set geometry = ST_SetSRID(geometry, 0)
+where st_srid(t2018_kar_buildings.geometry) != 0;
+
+
+update t2019_kar_poi_table
+set geometry = ST_SetSRID(geometry, 0)
+where st_srid(t2019_kar_poi_table.geometry) != 0;
+
+update t2018_kar_poi_table
+set geometry = ST_SetSRID(geometry, 0)
+where st_srid(t2018_kar_poi_table.geometry) != 0;
 
 
 Select poi.type, count(poi.*) from (select poi2019.* from t2019_kar_poi_table poi2019
@@ -20,8 +41,17 @@ join
 (select * from t2019_kar_buildings
 except
 select * from t2018_kar_buildings) new_buildings
-on ST_WITHIN(poi.geometry,st_buffer(new_buildings.geometry, 500))
+on ST_WITHIN(st_setsrid(poi.geometry,25832),st_buffer(st_setsrid(new_buildings.geometry,25832), 500))
 group by poi.type;
+
+
+
+
+
+
+
+
+
 
 --3
 
@@ -67,9 +97,9 @@ where st_srid(t2019_kar_street_node.geometry) != 3068;
 
 --6
 select nodes.* from t2019_kar_street_node nodes
-where st_within(nodes.geometry, st_buffer(st_makeline
-      ((select poi.geometry from input_points poi where poi.name like 'A' limit 1),
-      (select poi.geometry from input_points poi where poi.name like 'B' limit 1)),200)
+where st_within(st_transform(nodes.geometry,25832), st_buffer(st_makeline
+      ((select st_transform(poi.geometry,25832) from input_points poi where poi.name like 'A' limit 1),
+      (select st_transform(poi.geometry,25832) from input_points poi where poi.name like 'B' limit 1)),200)
        );
 
 --7
@@ -83,10 +113,13 @@ update t2019_kar_poi_table
 set geometry = ST_SetSRID(geometry, 3068)
 where st_srid(t2019_kar_poi_table.geometry) != 3068;
 
-select count(poi.*) as number_of_shops from t2019_kar_poi_table poi
-join t2019_kar_land_use_a as land
-on st_dwithin(poi.geometry, land.geometry,300)
-where poi.type like 'Sporting Goods Store';
+select count(*) as number_of_shops
+from t2019_kar_poi_table as poi
+where poi.type like 'Sporting Goods Store'
+  and ST_DWithin(ST_Transform(poi.geometry, 25832),(SELECT ST_Union(ST_Transform(land.geometry, 25832))FROM t2019_kar_land_use_a AS land WHERE land.type ILIKE '%park%'),
+        200
+      );
+
 
 
 
@@ -103,6 +136,8 @@ create table input_points
     geometry geometry,
     name     varchar
 );
+drop table T2019_KAR_BRIDGES;
 
-select st_intersection(water.geometry, rails.geometry) as geometry into T2019_KAR_BRIDGES from t2019_kar_railways rails, t2019_kar_water_lines water
-where st_intersects(water.geometry,rails.geometry)
+
+select distinct st_intersection(water.geometry, rails.geometry) as geometry into T2019_KAR_BRIDGES from t2019_kar_railways rails
+join t2019_kar_water_lines water on st_intersects(water.geometry,rails.geometry)
